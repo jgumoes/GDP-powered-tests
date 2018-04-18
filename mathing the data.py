b455f9778410f2c23bcac130e57b084591af5bf7
Jack Gumoes <j.gumoes@gmail.com>
2018-04-18 01:53:54 +0100

    Turns out the code was only half finished. Finished and polished the PWM detector, added some stats, and made it so the code can automatically crawl through a folder and open the subfolders that contain the results, and process all of the results from each test at once. The final values are saved as a text file, because two hours of coding seemed like less effort than 5 minutes of copy-pasting. I have the best decisions, you can ask anyone

--- mathing the data.py
+++ mathing the data.py
@@ -9,21 +9,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from scipy import signal
+from numba import jit
 
 v_ranges = [0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20]
 
 path = r"C:\Users\Jack\Documents\Uni\GDP\Picoscope\Low Gear\36.01V, 0.48A, 2.67V\36.01V, 0.48A, 2.67V_01.txt"
 
+out_path="Low Gear.txt"
+out_file = None
+#@jit
+def do_all(folder_list):
+    """does all of the results in a folder. folder_list needs be a list of 
+    paths that the results are in"""
+    folder_list = sorted(folder_list)
+    check = input("the output filepath is %s, \nContinue?" % out_path)
+    if check[0] == "y":
+        pass
+    else:
+        return "Cancelled"
+    if out_path is not None:
+        global out_file
+        out_file = open(out_path, 'w')
+    for i in folder_list:
+        open_dir(i)
+    out_file.close()
+
+#@jit
+def open_dir(path, us=True):
+    """process all the data files in a directory"""
+    files = os.listdir(path)
+    if path.split("\\")[-1] == "":
+        pass
+    else:
+        path += "\\"
+    periods = np.array([])
+    t_on = np.array([])
+    t_off = np.array([])
+    w = 0
+    for i in files:
+        data = open_file(path+i, us=us)
+        out = level_shift(data, plot=False)
+        periods = np.append(periods, out[0])
+        t_on = np.append(t_on, out[1])
+        t_off = np.append(t_off, out[2])
+        w += out[3]
+        #print(periods, t_on, t_off)
+    print(path.split("\\")[-2])
+    p_stats = stats(periods)
+    print("Periods:\t", p_stats)
+    t_on_stats = stats(t_on)
+    print("On times:\t", t_on_stats)
+    t_off_stats = stats(t_off)
+    print("Off times:\t", t_off_stats)
+    w *= (1/len(files))
+    print("Angular Velocity:\t", w)
+    if out_file is not None:
+        line = ""
+        for i in path.split("\\")[-2].split(", "):
+            line += i[:-1] + ", "
+        line += str(w)+", "
+        line+="Periods: "+np.array2string(p_stats).replace('\n', '')+", "
+        line+="On_times: "+np.array2string(t_on_stats).replace('\n', '')+", "
+        line+="Off_times: "+np.array2string(t_off_stats).replace('\n', '')+"\n"
+        out_file.write(line)
+
+#@jit
 def open_file(path, us=True):
-    try:
-        path
-    except SyntaxError:
-        path = "%r" % path # stops the character escapes from happening
+    #try:
+    #    path
+    #except SyntaxError:
+    #    path = "%r" % path # stops the character escapes from happening
     file = np.loadtxt(path, skiprows=2)
     file[:, 0] -= file[0,0]
     f = open(path)
     f.readline()
     line = f.readline()
+    f.close()
     if line.split()[0] == "(ms)":
         if us is False:
             file[:, 0] = file[:, 0]/1000
@@ -46,11 +107,12 @@ def open_file(path, us=True):
         print("Time value not known")
         return line
 
-
+#@jit
 def find_diff(res):
     k1 = res[:-1]
     return res[1:]-k1
 
+#@jit
 def level_shift(data, threshold=10000, plot=False, us=None):
     """finds the pwm frequency and duty cycle. This one works by subtracting
     a low-pass filtered set of results from the results, so that the on states
@@ -68,7 +130,8 @@ def level_shift(data, threshold=10000, plot=False, us=None):
             us = False
     if us is True:
         threshold *= (1/1000000)
-    print(threshold)
+    time = time.astype(float)
+    #print(threshold)
     val = data[:, 1]
     N = len(val)
     fs = 1/np.average(np.abs(find_diff(time)))
@@ -80,8 +143,8 @@ def level_shift(data, threshold=10000, plot=False, us=None):
     mags = np.abs(np.fft.rfft(val))
     #plt.semilogx(freqs, mags)
     #plt.plot(freqs[1:], mags[1:])
-    w = 2*np.pi*freqs[1+np.argmax(mags[1:]*(freqs[1:]<threshold))] # angular velocity of the wheel
-    pwm = freqs[1+np.argmax(mags[1:]*(freqs[1:]>threshold))]
+    w = 2*np.pi*freqs[1+np.argmax(mags[1:]*(freqs[1:len(mags)]<threshold))] # angular velocity of the wheel
+    #pwm = freqs[1+np.argmax(mags[1:]*(freqs[1:]>threshold))]
     cutoff = 3*np.round(w)*2/1000000
     b, a = signal.butter(1, cutoff, btype="low")
     res = val - signal.lfilter(b, a, val)*2/3   # level-shift proprtional to magnitude
@@ -103,21 +166,39 @@ def level_shift(data, threshold=10000, plot=False, us=None):
     periods = periods*(periods<(1/threshold))
     periods = periods[np.nonzero(periods)]
     #return periods
+    length = min([len(rising), len(falling)])
+    rising = rising[:length]
+    falling = falling[:length]
     if time[0]>0:
-        print(rising[0], falling[0])
-        t_on = falling - rising
-        t_off = falling[1:]-rising[:-1]
+        #print(rising[0], falling[0])
+        t_on = np.abs(falling - rising)
+        t_off = np.abs(falling[1:]-rising[:-1])
     else:
-        print("f")
-        t_on = falling[1:]-rising[:-1]
-        t_off = falling-rising
+        #print("f")
+        t_on = np.abs(falling[1:]-rising[:-1])
+        t_off = np.abs(falling-rising)
     t_on = t_on*(t_on<(1/threshold))
     t_on = t_on[np.nonzero(t_on)]
     t_off = t_off*(t_off<(1/threshold))
     t_off = t_off[np.nonzero(t_off)]
-    return periods, t_on, t_off
-    
-    
+    if us is True:
+        w*=1000000
+    return periods, t_on, t_off, w
+
+#@jit
+def stats(values):
+    """does that statistical analysis. returns the min, 1st quartile,
+    mean, 2nd quartile, max, and standard error.\n
+    accepts a 1-d array, and filters out outliers"""
+    h = np.histogram(values, "fd")
+    pos = np.nonzero(h[0]>(h[0].max()/20))[0]
+    out=np.zeros(len(values))
+    for i in pos:
+        out+=values*((values >=h[1][i]) & (values <= h[1][i+1]))
+    out = out[np.nonzero(out)]
+    return np.append(np.percentile(out, [0, 25, 50, 75, 100]), np.std(out)/np.sqrt(len(out)))
+
+#@jit
 def test_filt(fc=30000, D=0.8, res=10, N=10, f=15000):
     m = 2**res
     SR = f * m
